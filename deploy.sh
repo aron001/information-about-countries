f6fbfa1d5bde028b38eb689d50dfb392320fdabf
Aron Hunde <aronhunde20@gmail.com>
2023-10-24 22:16:05 +0300

    some change

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,28 @@
-#!/bin/sh
-current_date_time=$(date)
+#!/bin/bash
+
+# Build the React app
+npm run build
+
+# Create a new branch named "gh-pages"
+git checkout -b gh-pages
+
+# Remove existing files in the branch
+git rm -rf .
+
+# Copy the build files to the branch
+cp -r build/* .
+
+# Stage the changes
 git add .
-git commit -m "deploy $current_date_time" 
-git push
-npm run deploy
+
+# Commit the changes
+git commit -m "Deploy app to GitHub Pages"
+
+# Push the changes to the "gh-pages" branch
+git push origin gh-pages --force
+
+# Switch back to the main branch
+git checkout main
+
+# Delete the "gh-pages" branch
+git branch -D gh-pages
