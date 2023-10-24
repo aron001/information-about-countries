75e6f0b2312f43e10dcf3e8d90958e96a1be7166
Aron Hunde <aronhunde20@gmail.com>
2023-10-24 12:45:46 +0300

    test and deploy

--- test-and-deploy.sh
+++ test-and-deploy.sh
@@ -19,15 +19,11 @@ if [ $? -eq 0 ]; then
   git commit -m "Deploy to GitHub Pages"
 
   # Set the remote repository URL
-  # Replace `username` with your GitHub username and `repository` with your repository name
   git remote add origin https://github.com/aron001/information-about-countries.git
+  # Push the build files to the gh-pages branch
+  git push origin HEAD:gh-pages --force
 
-  # Push the build directory to the `gh-pages` branch
-  git push --force origin HEAD:gh-pages
-
-  # Clean up
-  cd ..
-  rm -rf build
-else
-  echo "Tests failed. Deployment aborted."
+  # Extract the deployment link
+  DEPLOYMENT_LINK="https://aron001.github.io/information-about-countries/"
+  echo "Deployment Link: $DEPLOYMENT_LINK"
 fi
